ae65b7acf51cda07192ed10a9eb931cdc23b5282
EC2 Default User <ec2-user@ip-172-31-44-141.ec2.internal>
2020-11-17 05:10:23 +0000

    [fix]plofile_iage method

--- Gemfile
+++ Gemfile
@@ -67,3 +67,5 @@ gem "refile-mini_magick"
 gem 'jquery-rails'
 
 gem 'bootstrap-sass', '~> 3.3.6'
+
+  gem 'pry-byebug'
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :books, dependent: :destroy
-  attachment :profile_image, destroy: false
+  attachment :profile_image
 
   validates :name, length: {maximum: 20, minimum: 2}, presence: true
 end
